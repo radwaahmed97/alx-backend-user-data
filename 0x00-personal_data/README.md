ff4227656979266047a4abacffa51a936e0b6bb4
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-29 16:37:27 -0400

    filtered_logger.py

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,1 @@
+READMEfile for this project
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""python module filtered logger"""
+
+import logging
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for f in fields:
+        trans = f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message
+        message = re.sub(trans)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
