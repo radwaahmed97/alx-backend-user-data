304529bc562e728c6c92e20a8718749f7d3c02de
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-03 16:45:51 -0400

    api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,14 @@ class BasicAuth(Auth):
             return reversed_base64.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """Extract user credentials"""
+        if decoded_base64_authorization_header is None or\
+           type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        user_credentials = decoded_base64_authorization_header.split(':', 1)
+        return (user_credentials[0], user_credentials[1])
