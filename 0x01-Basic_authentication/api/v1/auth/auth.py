21c60280e8f5fa9547efbac4fd5bb9a70109dbab
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-03 15:49:47 -0400

    auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Module of Auth views
+"""
+
+from models.user import User
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        return None
