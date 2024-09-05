501e5f10f3aaaaa4e5dda58246268414b3610c3c
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-05 14:49:02 -0400

    implementing session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""class SessionAuth that inherits from Auth"""
+
+Auth = __import__('api.v1.auth.auth').Auth
+
+
+class SessionAuth(Auth):
+    """class SessionAuth that inherits from Auth"""
+    pass
