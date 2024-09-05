8ef30145eaac015499cdf8747f5cda1e35984567
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-05 15:42:11 -0400

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,10 +33,11 @@ def before_request_func():
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if not auth.authorization_header(request):
+    auth_session_cookie = auth.session_cookie(request)
+    if not auth.authorization_header(request) and not auth_session_cookie:
         abort(401)
     if not auth.current_user(request):
         abort(403)
