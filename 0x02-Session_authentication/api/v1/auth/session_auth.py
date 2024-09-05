cdfef79765f1680e6ba4b373e1f048a670599412
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-05 14:57:11 -0400

    creating session id and linked it with user_is'

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,19 @@
 from models.user import User
 from api.v1.auth.auth import Auth
 from typing import TypeVar, List
+import uuid
 
 
 class SessionAuth(Auth):
     """class SessionAuth that inherits from Auth"""
-    pass
+    def __init__(self):
+        """attributes"""
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """method creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
