25fa4018180164f8f460ecb3322248e85621a2ff
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-05 14:52:15 -0400

    updated pycodestyle of session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ from models.user import User
 from api.v1.auth.auth import Auth
 from typing import TypeVar, List
 
+
 class SessionAuth(Auth):
     """class SessionAuth that inherits from Auth"""
     pass
