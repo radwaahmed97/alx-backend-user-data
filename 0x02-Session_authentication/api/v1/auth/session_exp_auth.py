7d66fc1136d0196562b0eca47a39e65e30242872
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-05 20:33:54 -0400

    updates

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -20,8 +20,8 @@ class SessionExpAuth(SessionAuth):
         """create a session"""
         session_id = super().create_session(user_id)
         if session_id:
-            session_dict = {'user_id': user_id, 'created_at': datetime.now()}
-            SessionAuth.user_id_by_session_id[session_id] = session_dict
+            SessionAuth.user_id_by_session_id[session_id] = {
+                'user_id': user_id, 'created_at': datetime.now()}
             return session_id
 
     def user_id_for_session_id(self, session_id=None):
@@ -32,10 +32,10 @@ class SessionExpAuth(SessionAuth):
         if not session_dict:
             return None
         if self.session_duration <= 0:
-            return session_dict.get('user_id')
+            return session_dict['user_id']
         if 'created_at' not in session_dict:
             return None
         delta_time = timedelta(seconds=self.session_duration)
         if session_dict['created_at'] + delta_time < datetime.now():
             return None
-        return session_dict.get('user_id')
+        return session_dict['user_id']
