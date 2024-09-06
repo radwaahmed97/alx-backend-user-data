56c7579d68d8a21587f064945675986a783779d3
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-05 20:24:16 -0400

    updates

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -11,12 +11,10 @@ class SessionExpAuth(SessionAuth):
     """class implementation to add expiration date to a Session ID"""
     def __init__(self):
         """constructor"""
-        self.session_duration = int(getenv('SESSION_DURATION'))
-        if not self.session_duration:
+        try:
+            self.session_duration = int(getenv('SESSION_DURATION'))
+        except Exception:
             self.session_duration = 0
-        if not isinstance(self.session_duration, int):
-            self.session_duration = 0
-        super().__init__()
 
     def create_session(self, user_id=None):
         """create a session"""
@@ -24,14 +22,14 @@ class SessionExpAuth(SessionAuth):
         if not session_id:
             return None
         session_dict = {'user_id': user_id, 'created_at': datetime.now()}
-        self.user_id_by_session_id[session_id] = session_dict
+        SessionAuth.user_id_by_session_id[session_id] = session_dict
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """return a User ID based on a Session ID"""
         if not session_id:
             return None
-        session_dict = self.user_id_by_session_id.get(session_id)
+        session_dict = SessionAuth.user_id_by_session_id.get(session_id)
         if not session_dict:
             return None
         user_id = session_dict.get('user_id')
