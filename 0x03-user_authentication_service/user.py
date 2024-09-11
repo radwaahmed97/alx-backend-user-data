9378f2869288afaa6766216068016198d2c3e64d
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-11 13:16:28 -0400

    user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,22 +2,18 @@
 """script to learn sqlalchemy"""
 
 import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-
-# create an engine
-engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
 
 
-# create a new table
 class User(Base):
+    """implementing of User class declarative to users table"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
