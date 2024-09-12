2f1da8eaa3bce2c034756304abc2265720e27a71
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-12 12:58:45 -0400

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -39,3 +40,7 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def __generate_uuid(self) -> str:
+        """Generate a new UUID"""
+        return str(uuid.uuid4())
