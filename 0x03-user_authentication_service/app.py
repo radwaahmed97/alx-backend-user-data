0a270f44ce8efda00e0c77716f3a472160e25f76
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-12 12:44:37 -0400

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 """basic flask application"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def Home():
     """return a JSON payload of the form"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """implements the POST /users route"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "User already exists"}), 404
 
 
 if __name__ == "__main__":
