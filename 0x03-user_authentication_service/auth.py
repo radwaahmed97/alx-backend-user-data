203b8d95429b307bbd6d6eadef31bf02d05314f5
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-12 12:56:24 -0400

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,3 +27,15 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        check the password with bcrypt.checkpw.
+        If it matches return True. In any other case, return False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
