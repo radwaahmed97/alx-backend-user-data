76e6939462fc5bf02e9f6ae489847e52a6b3652b
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-12 13:01:22 -0400

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,6 +13,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid(self) -> str:
+    """Generate a new UUID"""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -40,7 +45,3 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
-
-    def __generate_uuid(self) -> str:
-        """Generate a new UUID"""
-        return str(uuid.uuid4())
